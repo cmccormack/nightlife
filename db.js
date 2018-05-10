3d802200ef78982d1941a67a8eee4cbc8cb132ef
cmccormack <mccormack.christopher@gmail.com>
2018-05-10 12:57:54 -0700

    Initial Commit

---
+++ db.js
@@ -0,0 +1,12 @@
+const path = require("path")
+require("dotenv").config({path: path.resolve(__dirname, ".env"),})
+
+const { dbuser, dbpw, dbhost, dbname, } = process.env
+module.exports = {
+  url: `mongodb://${dbhost}/${dbname}`,
+  db: dbname,
+  options: {
+    user: dbuser,
+    pass: dbpw,
+  },
+}
