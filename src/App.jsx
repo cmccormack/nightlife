833e91a732cb10c9c1ff48b6805126e9ecc46ee9
Christopher McCormack <mccormack.christopher@gmail.com>
2018-05-13 20:00:07 -0700

    Lots of work with Material UI, added header/footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,67 @@
 import React, { Component, } from "react"
+import PropTypes from "prop-types"
+import { injectGlobal, } from "styled-components"
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider"
+import blueGrey from "@material-ui/core/colors/blueGrey"
+import IconButton from "@material-ui/core/IconButton"
+import { AccountCircle, } from "@material-ui/icons"
+import { withStyles, } from "@material-ui/core/styles"
+import FontAwesomeIcon from "@fortawesome/react-fontawesome"
+import {
+  faFacebookF, 
+  faTwitter,
+  faLinkedinIn,
+  faGithubAlt,
+} from "@fortawesome/fontawesome-free-brands"
 
+import theme from "./theme"
+import { Header, Footer, } from "./views/layout"
+import { Typography, } from "@material-ui/core"
+
+
+injectGlobal`
+  body {
+    background-color: ${blueGrey[100]};
+  }
+`
+
+const styles = {
+  body: {
+    display: "flex",
+    minHeight: "100vh",
+    flexDirection: "column",
+  },
+  main: {
+    flex: "1 0 auto",
+  },
+  textLink: {
+    textDecoration: "none",
+    color: theme.palette.text.primary,
+  },
+}
+
+const globalOptions = {
+  footer: {
+    socialIcons: [
+      {
+        href: "https://www.facebook.com/christopher.j.mccormack",
+        icon: faFacebookF,
+      },
+      {
+        href: "https://twitter.com/chrisjmccormack",
+        icon: faTwitter,
+      },
+      {
+        href: "https://github.com/cmccormack",
+        icon: faGithubAlt,
+      },
+      {
+        href: "https://www.linkedin.com/in/christopherjmccormack",
+        icon: faLinkedinIn,
+      },
+    ],
+  },
+}
 
 class App extends Component {
 
@@ -15,11 +77,62 @@ class App extends Component {
 
 
   render() {
+    const { classes, } = this.props
+
     return (
-      <h1>{this.state.text}</h1>
+      <MuiThemeProvider theme={ theme }>
+        <div className={classes.body}>
+          <Header
+            title="Meet Up Tonight"
+            items={[
+              <IconButton
+                color="inherit"
+                key="login"
+              >
+                <AccountCircle />
+              </IconButton>,
+            ]}
+          />
+          <main className={classes.main}>
+            <h1>{this.state.text}</h1>
+          </main>
+          <Footer
+            items={globalOptions.footer.socialIcons.map(item => (
+              <IconButton
+                href={item.href}
+                key={ item.icon.iconName }
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <FontAwesomeIcon
+                  icon={ item.icon }
+                  // fontSize="1.2 rem"
+                />
+              </IconButton>
+            ))}
+          >
+            <Typography
+              variant="body1"
+            >
+              {"Created by "}
+              <a
+                className={classes.textLink}
+                href="https://mackville.net"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {"Christopher McCormack"}
+              </a>
+            </Typography>
+          </Footer>
+        </div>
+      </MuiThemeProvider>
     )
   }
-
 }
 
-export default App
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(App)
