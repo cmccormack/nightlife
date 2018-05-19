43982790593aafdaccbc61255d26d9665004ab01
Christopher McCormack <mccormack.christopher@gmail.com>
2018-05-19 12:49:03 -0700

    Fixed up Layout, added input, added AppContext AppConsumer

--- src/views/components/SearchBarForm.jsx
+++ src/views/components/SearchBarForm.jsx
@@ -1,39 +1,95 @@
 import React from "react"
+import PropTypes from "prop-types"
 import Grid from "@material-ui/core/Grid"
 import Paper from "@material-ui/core/Paper"
 import Typography from "@material-ui/core/Typography"
+import Input from "@material-ui/core/Input"
+import FormControl from "@material-ui/core/FormControl"
+import InputAdornment from "@material-ui/core/InputAdornment"
+import InputLabel from "@material-ui/core/InputLabel"
+import IconButton from "@material-ui/core/IconButton"
+import GpsNotFixed from "@material-ui/icons/GpsNotFixed"
+import GpsFixed from "@material-ui/icons/GpsFixed"
 import withStyles from "@material-ui/core/styles/withStyles"
+import cx from "classnames"
+
+import { AppConsumer, AppProvider, } from "../contexts/AppContext"
 
 
 const styles = theme => ({
   searchBar: {
-
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexWrap: "wrap",
+    width: "100%",
   },
   paper: {
+    flex: "1 0 auto",
     padding: theme.spacing.unit * 2,
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  headline: {
+    fontWeight: theme.typography.fontWeightLight,
     textAlign: "center",
-    color: theme.palette.text.secondary,
+  },
+  textField: {
+    flexBasis: 450,
   },
 })
 
 class SearchBarForm extends React.Component {
 
+  static propTypes = {
+    classes: PropTypes.object,
+  }
+
   render() {
 
-    const { children, classes, } = this.props
-
+    const { classes, } = this.props
     return (
-      <Grid
-        className={ classes.searchBar } 
-        item xs={12}
-      >
-        <Paper className={ classes.paper }>
-          <Typography variant="body2">
-            { children }
-            some text
-          </Typography>
-        </Paper>
-      </Grid>
+      <Paper className={classes.paper}>
+        <Grid container direction="row">
+          <AppConsumer>
+            {({ handleLocationChange, location, }) => (
+              <form noValidate className={classes.searchBar}>
+                <Typography
+                  variant="headline"
+                  className={classes.headline}
+                >
+                  Enter Location or Find Me Automatically!
+                </Typography>
+                <FormControl
+                  fullWidth
+                  className={cx(classes.margin, classes.textField)}
+                >
+                  <InputLabel htmlFor="adornment-gps">Location</InputLabel>
+                  <Input
+                    id="adornment-gps"
+                    type="text"
+                    value={location}
+                    onChange={handleLocationChange}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="Request GPS Location"
+                          // onClick={this.handleClickShowPassword}
+                          // onMouseDown={this.handleMouseDownPassword}
+                        >
+                          <GpsNotFixed />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </form>
+            )}
+          </AppConsumer>
+        </Grid>
+      </Paper>
     )
   }
 }
