7bae461279c964d5243ebb08196aa1a38ddac439
Christopher McCormack <mccormack.christopher@gmail.com>
2018-05-20 16:44:52 -0700

    Added various search handlers

--- src/views/contexts/AppContext.jsx
+++ src/views/contexts/AppContext.jsx
@@ -8,11 +8,60 @@ export class AppProvider extends React.Component {
 
   state = {
     isAuth: false,
+    locationFound: false,
     location: "",
+    term: "night life",
+  }
+
+  fetchSearchResults = params => {
+    const esc = encodeURIComponent
+    const query = Object.keys(params)
+      .map(k => esc(k) + "=" + esc(params[k]))
+      .join("&")
+
+    console.log(query)
+    fetch(`/api/search?${query}`)
+      .then(res => res.json())
+      .then(({response,}) => {
+        const { city, state, } = response.jsonBody.businesses[0].location
+        this.setState({location: `${city}, ${state}`,})
+      }).catch(console.error)
   }
 
   handleLocationChange = e => {
     this.setState({ location: e.target.value, })
+  }
+
+  handleLocationFormSubmit = e => {
+    e.preventDefault()
+
+    const { location, term, } = this.state
+    this.fetchSearchResults({ location, term, })
+
+  }
+
+  handleGeolocate = e => {
+    e.preventDefault()
+    const { term, } = this.state
+
+    const handleSuccess = ({ coords: { latitude: lat, longitude: lon, },}) => {
+      const location = `${lat}, ${lon}`
+      this.setState({
+        location,
+        locationFound: true,
+      })
+      this.fetchSearchResults({ location, term,})
+    }
+
+    const handleFailure = error => {
+      console.error(error)
+    }
+
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleFailure)
+  }
+
+  handleRequestLocation = () => {
+    this.setState({ locationFound: true, })
   }
 
   static propTypes = {
@@ -25,7 +74,10 @@ export class AppProvider extends React.Component {
       <AppContext.Provider
         value={{
           ...this.state,
+          handleGeolocate: this.handleGeolocate,
           handleLocationChange: this.handleLocationChange,
+          handleLocationFormSubmit: this.handleLocationFormSubmit,
+          handleRequestLocation: this.handleRequestLocation,
         }}
       >
         { this.props.children }
