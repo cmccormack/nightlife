b0552d1e5e6273092eb92d7cfb860b56b60734e4
Christopher McCormack <mccormack.christopher@gmail.com>
2018-05-23 10:41:18 -0700

    added switchCase tabbing

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     "parser": "babel-eslint",
     "plugins": [ "react" ],
     "rules": {
-        "indent": [ "error", 2 ],
+        "indent": [ "error", 2, { "SwitchCase": 1 } ],
         "quotes": [ "error", "double" ],
         "semi": [ "error", "never" ],
         "no-console": [
