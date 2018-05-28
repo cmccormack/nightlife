336983d945120e6fc6a3d480be9b96e830e66ffe
Christopher McCormack <mccormack.christopher@gmail.com>
2018-05-27 18:07:32 -0700

    Changed going to required

--- models/Location.js
+++ models/Location.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const locationSchema = new mongoose.Schema({
   _id: {type: String, required: true, },
   name: {type: String, required: true, },
-  going: Number,
+  going: {type: Number, required: true, },
 })
 
 const Location = mongoose.model("Location", locationSchema, "locations")
