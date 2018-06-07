e8cbe9ac5c26ead87289b25af3e2ddc9ac32d3c9
cmccormack <mccormack.christopher@gmail.com>
2018-06-06 19:40:01 -0700

    Added /logout route

--- routes/twitter.js
+++ routes/twitter.js
@@ -2,6 +2,12 @@ const router = require("express").Router()
 
 module.exports = (passport) => {
 
+  router.get("/logout", (req, res, next) => {
+    console.log("Logging out user " + req.user)
+    req.logout()
+    res.redirect("/")
+  })
+
   router.get("/twitter", passport.authenticate("twitter"))
 
   router.get("/twitter/callback",
@@ -11,6 +17,7 @@ module.exports = (passport) => {
     })
   )
 
+  /* eslint no-unused-vars: 0 */
   router.get("/twitter/fail", (req, res, next) => {
     res.send("<h1>Failed to Log in to Twitter</h1>")
   })
