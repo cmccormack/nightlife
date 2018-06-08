6f98334f8a72744badcc095f0123bdfa7381cbc4
cmccormack <mccormack.christopher@gmail.com>
2018-06-07 22:14:43 -0700

    Removed all query params other than location

--- routes/twitter.js
+++ routes/twitter.js
@@ -1,31 +1,37 @@
 const router = require("express").Router()
+const queryString = require("query-string")
 
 module.exports = (passport) => {
 
-  router.get("/logout", (req, res, next) => {
+  router.get("/logout", (req, res) => {
     req.logout()
     // res.json({ success: !req.isAuthenticated(), })
     res.redirect("/")
   })
 
+  
   router.get("/twitter", (req, res, next) => {
-    const { location="", } = req.query
     passport.authenticate("twitter", {
-      callbackURL: `/auth/twitter/callback?location=${location}`,
+      callbackURL: `/auth/twitter/callback?${queryString.stringify(req.query)}`,
     })(req, res, next)
   })
+
 
   router.get("/twitter/callback", (req, res, next) => {
-    const { location="", } = req.query
+    
     passport.authenticate("twitter", {
-      failureRedirect: "/auth/twitter/fail",
-      successRedirect: `/?location=${location}`,
+      failureRedirect: "./fail",
+      successRedirect: `./success?${queryString.stringify(req.query)}`,
     })(req, res, next)
-
+    
   })
 
-  /* eslint no-unused-vars: 0 */
-  router.get("/twitter/fail", (req, res, next) => {
+  router.get("/twitter/success", (req, res) => {
+    const query = queryString.stringify({location: req.query.location,})
+    res.redirect(`/?${query}`)
+  })
+
+  router.get("/twitter/fail", (req, res) => {
     res.send("<h1>Failed to Log in to Twitter</h1>")
   })
 
