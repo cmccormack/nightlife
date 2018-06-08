70937e39e0a84ab72c2423ed8813d5061ec49b81
cmccormack <mccormack.christopher@gmail.com>
2018-06-07 17:01:58 -0700

    Added location prop to send query location to App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import queryString from "query-string"
 
 import App from "./App"
 import "./images/favicon.ico"
 import { AppProvider, } from "./views/contexts/AppContext"
 
+const queryLocation = queryString.parse(location.search).location
+
 ReactDOM.render(
-  <AppProvider>
+  <AppProvider location={queryLocation}>
     <App/>
   </AppProvider>,
   document.getElementById("root")
