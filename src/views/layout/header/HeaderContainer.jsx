358d442a28ebabb1963e2d0816fe62c5e2eecfe5
cmccormack <mccormack.christopher@gmail.com>
2018-06-08 18:28:45 -0700

    Moved presentation components from container to form, Added avatar and menu to form

--- src/views/layout/header/HeaderContainer.jsx
+++ src/views/layout/header/HeaderContainer.jsx
@@ -1,63 +1,20 @@
 import React, { Component, } from "react"
-import PropTypes from "prop-types"
-import IconButton from "@material-ui/core/IconButton"
-import queryString from "query-string"
 
 import { AppConsumer, } from "../../contexts/AppContext"
 import HeaderForm from "./HeaderForm"
-import AccountCircle from "@material-ui/icons/AccountCircle"
 
 class HeaderContainer extends Component {
 
-  static propTypes = {
-    loggedIn: PropTypes.bool.isRequired,
-    query: PropTypes.object,
-  }
-
-  static defaultProps = {
-    query: {},
-  }
-
   render() {
 
-    const title="Meet Up Tonight"
-
-    const { loggedIn, query, } = this.props
-    const items = (
-
-      !loggedIn
-        ? [
-          <IconButton
-            color="inherit"
-            href={`/auth/twitter?${queryString.stringify(query)}`}
-            key="login"
-          >
-            <AccountCircle />
-          </IconButton>,
-        ]
-        : [
-          <a href="/auth/logout" key="logout">Log Out</a>,
-        ]
-    )
-    
     return (
-      <HeaderForm
-        title={title}
-        items={items}
-        {...this.props}
-      />
+      <AppConsumer>
+        {
+          (app) => <HeaderForm {...this.props} {...app} />
+        }
+      </AppConsumer>
     )
   }
 }
 
-
-
-const HeadercontainerWithAppConsumer = props => (
-  <AppConsumer>
-    {
-      (app) => <HeaderContainer {...props} {...app} />
-    }
-  </AppConsumer>
-)
-
-export default HeadercontainerWithAppConsumer
+export default HeaderContainer
--- src/views/layout/header/HeaderForm.jsx
+++ src/views/layout/header/HeaderForm.jsx
@@ -3,14 +3,27 @@ import PropTypes from "prop-types"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
+import IconButton from "@material-ui/core/IconButton"
+import Menu from "@material-ui/core/Menu"
+import MenuItem from "@material-ui/core/MenuItem"
+import Avatar from "@material-ui/core/Avatar"
+import queryString from "query-string"
+import AccountCircle from "@material-ui/icons/AccountCircle"
 import withStyles from "@material-ui/core/styles/withStyles"
 
 const styles = {
   root: {
     flexGrow: 1,
   },
+  avatar: {
+    border: "2px solid white",
+  },
   flex: {
     flex: 1,
+  },
+  menu: {
+    top: "40px",
+    width: "200px",
   },
   textLink: {
     textDecoration: "none",
@@ -18,11 +31,86 @@ const styles = {
   },
 }
 
+
 class HeaderForm extends Component {
 
+  state = {
+    anchorEl: null,
+  }
+
+  static propTypes = {
+    classes: PropTypes.object,
+    loggedIn: PropTypes.bool.isRequired,
+    query: PropTypes.object,
+    user: PropTypes.object,
+  }
+
+  static defaultProps = {
+    classes: {},
+    query: {},
+    user: {
+      id: "userid",
+      profile_image: "",
+      displayName: "",
+    },
+  }
+
+  handleMenuOpen = (e) => {
+    this.setState({
+      anchorEl: e.currentTarget,
+    })
+  }
+
+  handleMenuClose = () => {
+    this.setState({
+      anchorEl: null,
+    })
+  }
+
+
   render() {
-    const { classes, title, items, } = this.props
-    
+
+    const { classes, loggedIn, query, } = this.props
+    const { id, profile_image, displayName, } = this.props.user
+
+    const { anchorEl, } = this.state
+
+    const title="Meet Up Tonight"
+
+    const items = (
+
+      !loggedIn
+        ? [
+          <IconButton
+            color="inherit"
+            href={`/auth/twitter?${queryString.stringify(query)}`}
+            key="login"
+          >
+            <AccountCircle />
+          </IconButton>,
+        ]
+        : [
+          <Avatar
+            alt={displayName}
+            className={classes.avatar}
+            key={id}
+            onClick={this.handleMenuOpen}
+            src={profile_image}
+          />,
+          <Menu
+            key="menu"
+            anchorEl={anchorEl}
+            className={classes.menu}
+            open={Boolean(anchorEl)}
+            onClose={this.handleMenuClose}
+          >
+            <MenuItem onClick={this.handleMenuClose}>Logout</MenuItem>
+          </Menu>,
+          <a href="/auth/logout" key="logout">Log Out</a>,
+        ]
+    )
+
+
     return (
       <header>
         <div className={ classes.root }>
@@ -37,9 +125,7 @@ class HeaderForm extends Component {
                   { title }
                 </a>
               </Typography>
-              { items.map(item => (
-                item
-              ))}
+              { items }
             </Toolbar>
           </AppBar>
         </div>
@@ -48,22 +134,4 @@ class HeaderForm extends Component {
   }
 }
 
-HeaderForm.propTypes = {
-  title: PropTypes.string,
-  classes: PropTypes.object,
-  items: PropTypes.array,
-}
-
-HeaderForm.defaultProps = {
-  classes: {
-    root: {
-      flexGrow: 1,
-    },
-    flex: {
-      flex: 1,
-    },
-  },
-  items: [],
-  title: "Title",
-}
 export default withStyles(styles)(HeaderForm)
