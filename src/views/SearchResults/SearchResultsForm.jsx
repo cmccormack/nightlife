6aa7aefc9ba7a28d35941f7767c95ab871db3c2c
cmccormack <mccormack.christopher@gmail.com>
2018-06-11 21:10:00 -0700

    Cleanup and moved grid item out of SearchResult

--- src/views/SearchResults/SearchResultsForm.jsx
+++ src/views/SearchResults/SearchResultsForm.jsx
@@ -1,21 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Typography from "@material-ui/core/Typography"
-import Button from "@material-ui/core/Button"
-import ThumbUp from "@material-ui/icons/ThumbUp"
-import Star from "@material-ui/icons/Star"
-import LocationOn from "@material-ui/icons/LocationOn"
-import Card from "@material-ui/core/Card"
-import CardMedia from "@material-ui/core/CardMedia"
-import CardContent from "@material-ui/core/CardContent"
-import Paper from "@material-ui/core/Paper"
 import Grid from "@material-ui/core/Grid"
 import withStyles from "@material-ui/core/styles/withStyles"
-import cx from "classnames"
 
-import SearchResult from './SearchResult'
+import SearchResult from "./SearchResult"
 
-const styles = theme => ({
+const styles = () => ({
   default_display: { 
     display: "flex",
     flexDirection: "column",
@@ -33,6 +24,12 @@ class SearchResultsForm extends React.Component {
 
   static propTypes = {
     classes: PropTypes.object,
+    searchResults: PropTypes.array,
+  }
+
+  static defaultProps = {
+    classes: {},
+    searchResults: [],
   }
 
   render() {
@@ -60,10 +57,12 @@ class SearchResultsForm extends React.Component {
         {
           searchResults.map(result => {
             return (
-              <SearchResult
-                {...result}
-                {...rest}
-              />
+              <Grid item xs={12} key={result.id}>
+                <SearchResult
+                  result={result}
+                  {...rest}
+                />
+              </Grid>
             )
           })
         }
