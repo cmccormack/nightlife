5892a6f1fe1b61353c57aee17fc268ccea6555c2
cmccormack <mccormack.christopher@gmail.com>
2018-06-11 22:43:12 -0700

    Added logic for loading animation and created HoC for getting AppConsumer props

--- src/views/SearchBar/SearchBarPage.jsx
+++ src/views/SearchBar/SearchBarPage.jsx
@@ -3,16 +3,43 @@ import React from "react"
 import { AppConsumer, } from "../contexts/AppContext"
 import SearchBarForm from "./SearchBarForm"
 
-export default class SearchBarPage extends React.Component {
+class SearchBarPage extends React.Component {
+
+  state = {
+    loading: false,
+  }
+
+  handleGeolocate = async (e) => {
+    e.preventDefault()
+    this.setState({loading: true})
+    await this.props.handleGeolocate()
+    this.setState({loading: false})
+  }
 
   render() {
 
+    const { handleGeolocate, ...props } = this.props
+    return (
+
+      <SearchBarForm
+        {...this.state}
+        {...props}
+        handleGeolocate={this.handleGeolocate} 
+      />
+    )
+  }
+}
+
+const withApp = function(Component) {
+  return function SearchBarPageWithApp(props) {
     return (
       <AppConsumer>
         {
-          (app) => <SearchBarForm {...this.props} {...app}/>
+          (app) => <Component {...props} {...app} />
         }
       </AppConsumer>
     )
   }
 }
+
+export default withApp(SearchBarPage)
