1d36f1c1f36ea58248f1c64d0010e90311dedc02
cmccormack <mccormack.christopher@gmail.com>
2018-06-12 10:36:43 -0700

    Added placeholder image import

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import queryString from "query-string"
 
 import App from "./App"
 import "./images/favicon.ico"
+import "./images/placeholder.jpg"
 import { AppProvider, } from "./views/contexts/AppContext"
 
 const query = location.search ? queryString.parse(location.search) : undefined
