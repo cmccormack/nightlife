f877fb7a43498df405e5efe4c19970f68800c000
cmccormack <mccormack.christopher@gmail.com>
2018-06-12 11:27:00 -0700

    Removed unused vars

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,6 @@
 const path = require("path")
-const express = require("express")
 const queryString = require("query-string")
 
-const Location = require("../models/Location")
 
 module.exports = (app, passport) => {
 
