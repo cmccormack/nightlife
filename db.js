c5a99d400ab9df7c8b72bf5aa25ec70d94a3c1bb
Christopher McCormack <mccormack.christopher@gmail.com>
2020-11-29 15:58:14 -0700

    migrated to mongodb atlas

--- db.js
+++ db.js
@@ -3,10 +3,12 @@ require("dotenv").config({path: path.resolve(__dirname, ".env"),})
 
 const { dbuser, dbpw, dbhost, dbname, } = process.env
 module.exports = {
-  url: `mongodb://${dbhost}/${dbname}`,
+  url: `mongodb+srv://${dbhost}/${dbname}`,
   db: dbname,
   options: {
     user: dbuser,
     pass: dbpw,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
   },
 }
